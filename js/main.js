ad2f744a6e51d9ede4ef911583b72c9818450c6d
Bugname <bugname26@gmail.com>
2019-10-11 10:12:31 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,55 +1,84 @@
 //---------------------------------------- Прокрутка экранов
-let scrollFlag = 0;
+let scrollFlag = 1;
 let screensCount = $('.container > div').length;
 
-$(window).bind('mousewheel', function(event) {
-    if(scrollFlag == 0){
-        scrollFlag = 1;
-        setTimeout(function () {
-            scrollFlag = 0;
-        }, 700);
+$(window).bind('mousewheel DOMMouseScroll', function(event) {
+    if(scrollFlag === 1){
         if (event.originalEvent.wheelDelta >= 0) {
             if($('.active').index() != 0){
                 $('.active').removeClass('active').addClass('gone-bottom').prev().removeClass('gone-top').addClass('active');
+                scrollFlag = 0;
+                setTimeout(function () {
+                    scrollFlag = 1;
+                }, 1500);
             }
         }
         else {
             if($('.active').index() != (screensCount - 1)){
                 $('.active').removeClass('active').addClass('gone-top').next().removeClass('gone-bottom').addClass('active');
+                scrollFlag = 0;
+                setTimeout(function () {
+                    scrollFlag = 1;
+                }, 1500);
             }
         }
     }
 });
-//-------------------------------------Появление лого
+//-------------------------------------Появление панели
 $(function () {
-    setTimeout(function () {
-        setTimeout(function () {
-            $('.letter1').addClass('shown');
-        },0);
-        setTimeout(function () {
-            $('.letter2').addClass('shown');
-        },120);
-        setTimeout(function () {
-            $('.letter3').addClass('shown');
-        },240);
-        setTimeout(function () {
-            $('.letter4').addClass('shown');
-        },360);
-        setTimeout(function () {
-            $('.letter5').addClass('shown');
-        },480);
-        setTimeout(function () {
-            $('.letter6').addClass('shown');
-        },600);
-        setTimeout(function () {
-            $('.letter7').addClass('shown');
-        },720);
-        setTimeout(function () {
-            $('.letter8').addClass('shown');
-        },840);
-        setTimeout(function () {
-            $('.letter9').addClass('shown');
-        },960);
-    }, 500);
+    setTimeout("$('.date').addClass('shown')", 3000);
+    setTimeout("$('.menu-button').addClass('shown')", 3400);
+    setTimeout("$('.rights').addClass('shown')", 3600);
+    setTimeout("$('.social').addClass('shown')", 4100);
 });
+
+$(function () {
+    //----------Открытие модального видео и закрытие
+    $('.play-button').click(function () {
+        $('.video-modal').removeClass('hidden');
+    });
+    $('.modal-close').click(function () {
+        $('.video-modal').addClass('hidden');
+        uppodSend("videoplayer","play");
+    });
+    //-------------Ховер блоков
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
